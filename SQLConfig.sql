b1a34327d59ebf80fc539fe84d921bd1a850655b
Gustavo de Oliveira Rosa <gustavoolrosa2019@gmail.com>
2020-08-23 23:54:07 -0300

    Adicao SQLConfig

---
+++ SQLConfig.sql
@@ -0,0 +1,30 @@
+-- CREATE DATABASE HSAK;
+
+USE  HSAK;
+
+/*
+ DROP TABLE usuarios;
+ CREATE TABLE usuarios (
+    id_usuario INT (11) NOT NULL AUTO_INCREMENT COMMENT 'ID Titulo',
+    usuario VARCHAR (255) NOT NULL COMMENT 'Nome',
+    senha VARCHAR (255) NOT NULL COMMENT 'Genero',
+    nivel_permissao VARCHAR (255) NOT NULL COMMENT 'Sinopse',
+     PRIMARY KEY (id_usuario)
+) CHARSET = utf8;
+
+TRUNCATE TABLE usuarios; 
+INSERT INTO usuarios (usuario,senha,nivel_permissao) values ("root","123","1");
+
+ CREATE TABLE aplicacoesWEB (
+    id_aplicacaoWEB INT (11) NOT NULL AUTO_INCREMENT COMMENT 'ID Titulo',
+     nome VARCHAR (255) NOT NULL COMMENT 'Nome',
+    url VARCHAR (255) NOT NULL COMMENT 'Url',
+    PRIMARY KEY (id_aplicacaoWEB )
+  ) CHARSET = utf8; 
+  
+insert into aplicacoesWEB (nome,url) values ("Google","google.com");
+  */
+insert into aplicacoesWEB (nome,url) values ("Facebook","facebook.com");
+insert into aplicacoesWEB (nome,url) values ("Gmail","gmail.com");
+insert into aplicacoesWEB (nome,url) values ("YouTube","youtube.com");
+  SELECT nome,url FROM aplicacoesWEB;
